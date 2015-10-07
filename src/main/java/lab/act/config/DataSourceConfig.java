e856625c37e0797bada999d8292f377172dccc39
jimwh <hiqusjz@gmail.com>
2015-10-06 21:09:11 -0400

    refactor

--- src/main/java/lab/act/config/DataSourceConfig.java
+++ src/main/java/lab/act/config/DataSourceConfig.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 @Configuration
-@PropertySources({@PropertySource("classpath:/db.properties")})
+@PropertySources({@PropertySource("classpath:/${env}.db.properties")})
 public class DataSourceConfig {
 
     @Autowired
